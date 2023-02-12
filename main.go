8007744c31a9fb1e164846f5523a2b464bae5e5c
Sarath T S <sarathiscookie@hotmail.com>
2023-02-12 23:09:27 +0530

    array slice and itereation

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	//
+	cards := [] string {"Five of Diamonds \n", sample()}
+
+	cards = append(cards, "Six of Spades \n")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func sample() string {
+	return "Ace of Diamonds"
 }
